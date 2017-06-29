54da540b717b1731d2681bb75460c344a9ec071c
Ireneusz Solek <ireneusz.solek@fyber.com>
2017-06-29 22:43:35 +0200

    framework

---
+++ RxFirebase/RxFirebase.h
@@ -0,0 +1,18 @@
+//
+//  RxFirebase.h
+//  RxFirebase
+//
+//  Created by Irek Sołek on 22/06/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxFirebase.
+FOUNDATION_EXPORT double RxFirebaseVersionNumber;
+
+//! Project version string for RxFirebase.
+FOUNDATION_EXPORT const unsigned char RxFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxFirebase/PublicHeader.h>
+
+
